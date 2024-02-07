67e10218da9bf7ebe17c8b7142d79310b2c6b763
sopand <pows1031@naver.com>
2024-02-07 14:39:33 +0900

    config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 		proxy: {
 			'/api': {
 				// 요청 전달 대상 서버 주소 설정
-				target: 'http://localhost:8091/',
+				target: 'http://13.209.179.204:8080/',
 			},
 		},
 	},
